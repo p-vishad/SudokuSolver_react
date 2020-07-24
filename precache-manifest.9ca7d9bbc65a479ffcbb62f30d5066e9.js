ef9c970963659fcd8dfadf74551c5bc8b5045e95
Vishad Pokharel <vishad@Vishads-MacBook-Pro.local>
2020-07-23 23:28:45 -0400

    Updates

---
+++ precache-manifest.9ca7d9bbc65a479ffcbb62f30d5066e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a9d4d08c9e6c26a15d13f1094e08260",
+    "url": "/SudokuSolver_react/index.html"
+  },
+  {
+    "revision": "0b8349f25ca0355845dc",
+    "url": "/SudokuSolver_react/static/css/main.8e7daf48.chunk.css"
+  },
+  {
+    "revision": "9416afa5d4819a91cc54",
+    "url": "/SudokuSolver_react/static/js/2.5c38978b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SudokuSolver_react/static/js/2.5c38978b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b8349f25ca0355845dc",
+    "url": "/SudokuSolver_react/static/js/main.aaafe182.chunk.js"
+  },
+  {
+    "revision": "6f61c26947a4c40bdfdc",
+    "url": "/SudokuSolver_react/static/js/runtime-main.674e95c4.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/SudokuSolver_react/precache-manifest.9ca7d9bbc65a479ffcbb62f30d5066e9.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SudokuSolver_react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
